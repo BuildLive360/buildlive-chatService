6ff613bc30e9a6d8a23e18572cd20e8d712281a5
Aravind <aravindmunna240@gmail.com>
2024-05-04 15:39:27 +0530

    company feign url

--- src/main/java/com/buildlive/websocket/service/feign/CompanyFeign.java
+++ src/main/java/com/buildlive/websocket/service/feign/CompanyFeign.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.UUID;
 
-@FeignClient(name = "COMPANY-SERVICE")
+@FeignClient(name = "COMPANY-SERVICE",url = "http://13.127.209.74:8010")
 public interface CompanyFeign {
 
         @GetMapping("/api/v1/company/{companyId}/get-partyMember-Id/{email}")
